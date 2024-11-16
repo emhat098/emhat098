8da424c5a6665d6df003522c037422bc645402e9
Em Ha Tuan <emhat098@gmail.com>
2024-11-16 14:13:50 +0700

    Change image to local in in-footer component.

--- components/footer/in-footer.tsx
+++ components/footer/in-footer.tsx
@@ -30,9 +30,7 @@ const InFooter = () => {
                 <FooterLinkItem
                   href='/'
                   title={'Home'}
-                  image={
-                    'https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-6/277796229_1219311105553639_7030352227871532204_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=duhDbQY36PAQ7kNvgHe-SRj&_nc_zt=23&_nc_ht=scontent.fsgn2-4.fna&_nc_gid=ApO9f2xRyQl0feS29YIbrrD&oh=00_AYC5j71DN3ibtwigNC4AvMebS2wlRNHhVuNwybOQY__H1A&oe=67365E3B'
-                  }
+                  image={'/home.jpg'}
                   description={'Go back home to see more things.'}
                 />
               </li>
@@ -40,9 +38,7 @@ const InFooter = () => {
                 <FooterLinkItem
                   href='/about'
                   title={'About me'}
-                  image={
-                    'https://scontent.fsgn2-10.fna.fbcdn.net/v/t39.30808-6/305050059_1320008258817256_7577732652959164136_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=x_orDqb7WKEQ7kNvgE7onTQ&_nc_zt=23&_nc_ht=scontent.fsgn2-10.fna&_nc_gid=ADUMoVe7wA9fcvqGqzKXqrt&oh=00_AYDBVFHGEzNPHj2GrzdhgQ2Osid74BnUZ4-PWaqJEeXRMg&oe=673652E3'
-                  }
+                  image={'/about-me.jpg'}
                   description={
                     'I believe the world needs more smiles than anger.'
                   }
@@ -60,9 +56,7 @@ const InFooter = () => {
                   }
                   href='/blog'
                   title={'My blogs'}
-                  image={
-                    'https://scontent.fsgn2-10.fna.fbcdn.net/v/t39.30808-1/454846648_1764741971010547_6440301084738931731_n.jpg?stp=c0.451.1536.1536a_dst-jpg_s320x320&_nc_cat=109&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=h5HDTzuwIyoQ7kNvgEV--nN&_nc_zt=24&_nc_ht=scontent.fsgn2-10.fna&_nc_gid=Ak4OOe4rqndtsleIGXRZhi9&oh=00_AYC1ZhDsY0HPyORrv0CsED-gazgcA2oXtikRz7uy4yYfMQ&oe=67365531'
-                  }
+                  image={'/my-blog.jpg'}
                 />
               </li>
             </ul>
